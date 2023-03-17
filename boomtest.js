18ccbf6b00cea4911a43f217b14aff547c9f64d0
leegunsun <talkcjstk@naver.com>
2023-03-17 21:36:14 +0900

    good

--- boomtest.js
+++ boomtest.js
@@ -10,6 +10,8 @@ const Boomtest = (req, res, next) => {
         .status(error.output.statusCode) // badRequest는 404이므로 404 statusCode를 반환합니다.
         .json({ errorMessage: error.output.payload.message }); // 에러 메시지를 설정하면 이쪽으로 빠집니다.
     } else {
+      console.log(`message : ${error.output.payload.message}`);
+      console.log(`statusCode : ${error.output.statusCode}`);
       res
         .status(500) // 설정하지 않은 에러는 500를 반환합니다.
         .json({ errorMessage: "설정하지 않은 에러는 여기로 빠집니다." });
