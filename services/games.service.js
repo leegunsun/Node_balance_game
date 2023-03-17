a8f4de8d2ec9aae222ee68287bf6200d16ac1e07
leegunsun <talkcjstk@naver.com>
2023-03-17 21:30:18 +0900

    boom

--- services/games.service.js
+++ services/games.service.js
@@ -6,9 +6,50 @@ class GamesService {
   }
 
   findAllGames = async () => {
-    const findAllGames = await this.gamesRepository.findAllGames();
+    try {
+      const findAllGames = await this.gamesRepository.findAllGames();
 
-    return findAllGames;
+      return findAllGames;
+    } catch (error) {
+      error = new Error("게임목록 조회에 실패하였습니다.");
+      error.status = 400;
+      throw error;
+    }
+  };
+
+  findOneGames = async (gameId) => {
+    try {
+      const findOneGame = await this.gamesRepository.findOneGame(gameId);
+
+      return findOneGame;
+    } catch (error) {
+      error = new Error("게임목록 조회에 실패하였습니다.");
+      error.status = 400;
+      throw error;
+    }
+  };
+
+  postGame = async (title, optionA, optionB, UserId) => {
+    if (!title && !optionA && !optionB) {
+      error = new Error("데이터 형식이 올바르지 않습니다.");
+      error.status = 400;
+      throw error;
+    }
+
+    if (!title) {
+      throw Boom.preconditionFailed("게시글 제목의 형식이 일치하지 않습니다.");
+    } else if (!optionA || !optionB) {
+      throw Boom.preconditionFailed("게시글 내용의 형식이 일치하지 않습니다.");
+    }
+
+    const createGame = await this.gamesRepository.createGame(
+      title,
+      optionA,
+      optionB,
+      UserId
+    );
+
+    return createGame;
   };
 }
 
