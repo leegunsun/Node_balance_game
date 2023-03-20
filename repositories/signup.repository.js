0bb2b98aefa323a18990b888326d62a1b704dea7
leegunsun <talkcjstk@naver.com>
2023-03-21 01:33:15 +0900

    싸인업레포고침

--- repositories/signup.repository.js
+++ repositories/signup.repository.js
@@ -3,9 +3,8 @@ const { Users } = require("../models");
 class SignupRepository {
   // 닉네임 중복 검사 함수
   isExistingNickname = async (nickname) => {
-    console.log(nickname);
     const user = await Users.findOne({ where: { nickname: nickname } });
-    console.log(user);
+
     return !!user;
   };
 
